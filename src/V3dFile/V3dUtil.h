55c9afad5a156c90fd8262f17f623f078d307c5f
Benjaming Bingham <ben@benbingham.ca>
2024-07-24 10:49:37 -0600

    Moved over most functionality from V3dGenerator to V3d-Common

--- src/V3dFile/V3dUtil.h
+++
@@ -1,4 +0,0 @@
-#include "xstream.h"
-#include "V3dTypes.h"
-
-float readReal(xdr::ixstream& xdrFile, BOOL doublePrecision);
--- src/v3d_generator.h
+++ src/v3d_generator.h
@@ -12,8 +12,8 @@
 
 #include <core/textdocumentgenerator.h>
 
-#include "renderheadless.h"
-#include "V3dFile/V3dFile.h"
+#include "3rdParty/V3D-Common/Rendering/renderheadless.h"
+#include "3rdParty/V3D-Common/V3dFile/V3dFile.h"
 
 #include <thread>
 #include "part/pageview.h"
@@ -21,6 +21,8 @@
 #include <core/generator.h>
 
 #include <chrono>
+
+#include "3rdParty/V3D-Common/V3dModelManager.h"
 
 class EventFilter;
 
@@ -30,80 +32,12 @@ class V3dGenerator : public Okular::Generator {
 
 public:
     V3dGenerator(QObject *parent, const QVariantList &args);
-    ~V3dGenerator() override;
+    ~V3dGenerator() override = default;
 
     void generatePixmap(Okular::PixmapRequest* request) override;
     bool loadDocument(const QString &fileName, QVector<Okular::Page*> &pages) override;
     bool doCloseDocument() override;
 
-    bool mouseMoveEvent(QMouseEvent* event);
-    bool mouseButtonPressEvent(QMouseEvent* event);
-    bool mouseButtonReleaseEvent(QMouseEvent* event);
-
 private:
-    bool m_MouseDown{ false };
-
-    enum class DragMode {
-        SHIFT,
-        ZOOM,
-        PAN,
-        ROTATE
-    };
-    DragMode m_DragMode{ DragMode::ROTATE };
-
-    glm::ivec2 m_MousePosition;
-    glm::ivec2 m_LastMousePosition;
-
-    float m_Zoom = 1.0f;
-    float m_LastZoom{ };
-
-    float xShift;
-    float yShift;
-
-    glm::vec2 m_PageViewDimensions;
-
-    glm::mat4 m_RotationMatrix{ 1.0f };
-    glm::mat4 m_ViewMatrix{ 1.0f };
-    glm::mat4 m_ProjectionMatrix{ 1.0f };
-
-    float m_H{ };
-    glm::vec3 m_Center{ };
-    glm::vec2 m_Shift{ };
-
-    struct ViewParam {
-        glm::vec3 minValues{ };
-        glm::vec3 maxValues{ };
-    };
-
-    ViewParam m_ViewParam;
-
-    void initProjection();
-    void setProjection();
-
-    void setDimensions(float width, float height, float X, float Y);
-    void updateViewMatrix();
-
-    void dragModeShift  (const glm::vec2& normalizedMousePosition, const glm::vec2& lastNormalizedMousePosition);
-    void dragModeZoom   (const glm::vec2& normalizedMousePosition, const glm::vec2& lastNormalizedMousePosition);
-    void dragModePan    (const glm::vec2& normalizedMousePosition, const glm::vec2& lastNormalizedMousePosition);
-    void dragModeRotate (const glm::vec2& normalizedMousePosition, const glm::vec2& lastNormalizedMousePosition);
-
-    void requestPixmapRefresh();
-
-    void refreshPixmap();
-
-    QAbstractScrollArea* getPageViewWidget();
-
-    static int m_V3dGeneratorCount;
-
-    std::unique_ptr<V3dFile> m_File{ nullptr };
-
-    std::chrono::duration<double> m_MinTimeBetweenRefreshes{ 1.0 / 100.0 }; // In Seconds
-    std::chrono::time_point<std::chrono::system_clock> m_LastPixmapRefreshTime;
-
-    HeadlessRenderer* m_HeadlessRenderer;
-
-    QAbstractScrollArea* m_PageView{ nullptr };
-
-    EventFilter* m_EventFilter{ nullptr };
+    V3dModelManager m_ModelManager;
 };
