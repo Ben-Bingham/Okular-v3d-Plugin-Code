f434915e7fd068da15fee5ca46bb226895f42cdd
Benjaming Bingham <ben@benbingham.ca>
2024-07-30 13:42:04 -0600

    Integrated new V3D-Common changes (shader paths)

--- src/v3d_generator.h
+++ src/v3d_generator.h
@@ -39,5 +39,5 @@ public:
     bool doCloseDocument() override;
 
 private:
-    V3dModelManager m_ModelManager{ document() };
+    V3dModelManager m_ModelManager{ document(), "kde/src/okular/generators/Okular-v3d-Embeded-Plugin-Code/3rdParty/V3D-Common/shaders/" };
 };
